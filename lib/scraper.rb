99a7ce0ce3130e25feb5faf7297241dea4b2b3b0
Doug_Jones <dsjr1284@gmial.com>
2020-04-25 22:20:46 -0400

    scraper update

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,18 +1,49 @@
 require 'pry'
-require 'open-uri'
- 
+require 'byebug'
+require "httparty"
     
-    
-     
-    def scraper
+# class SF
+#     attr_accessor :url 
+
+#     @@all=[]
+
+#     def initialize(url)
+#             @url = nil
+#             save
+#     end 
+        
+#     def save
+#      @@all << url
+#     end 
+        
+#         def self.all
+#             @@all
+#         end 
+
+#         def self.destory_all
+#             @@all.clear
+#         end 
+
+       def scraper
         url = "https://capcomprotour.com/standings"
         unparsed_page = HTTParty.get(url)
         parsed_page = Nokogiri::HTML(unparsed_page)
-        player_standings =parsed_page.css('div.player-details.clearfix')
-        player_standings.each do|ranks|
-          player_info ={ Name_location:parsed_page.css('p.details').text
-                        Gamer_tag_points:parsed_page.css('h3').text
-                        Players_url:parsed_page.css("a.player-link").to_a[0].attributes["href"].value }
-                    byebug
+        player_standings = parsed_page.css('div.standings-container')
+        sf_array = []
+        player_standings.each do|rank|       
+            player_info = { name_location: rank.css('p.details').text.split(/(?=[A-Z])/).join(" "),
+                            game_tag_points: rank.css('h3').text.split(/(?=[A-Z])/).join(" "),                       
+                            players_url: rank.css("a").attr("href").value
+                        }
+                           sf_array << player_info 
+                           sf_array  
+                binding.pry     
+        end 
          
-    end    
+    end   
+
+# end 
+    
+
+
+    
